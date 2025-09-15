b9906368a07c18d7d62aa80cd3af2a0abbca5372
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-09-15 14:36:24 +0600

    feat: user profile component added

---
+++ src/app/(WithDashboardLayout)/user/profile/page.tsx
@@ -0,0 +1,39 @@
+export const dynamic = "force-dynamic";
+
+import UserProfile from "@/components/dashboard/user/UserProfile";
+import { useUser } from "@/context/UserContext";
+import { cartSelector } from "@/redux/features/cartSlice";
+import { useAppSelector } from "@/redux/hooks";
+import { getCurrentUser } from "@/services/auth";
+import { getUserOrders } from "@/services/order";
+import { getSpecificUserProductReview } from "@/services/review";
+import { getSingleUser } from "@/services/user";
+import React from "react";
+
+const UserProfilePage = async () => {
+  const user = await getCurrentUser();
+
+  if (!user?.id) {
+    throw new Error("User is not logged in");
+  }
+
+  const { data: userInfo } = await getSingleUser(user.id);
+
+  const { data: userOrders } = await getUserOrders(user.id);
+
+  const sanitizedOrders = Array.isArray(userOrders) ? userOrders : [];
+
+  const { data: userReviews } = await getSpecificUserProductReview(user.id);
+
+  return (
+    <div className="flex justify-center items-center">
+      <UserProfile
+        user={userInfo}
+        orders={sanitizedOrders}
+        reviews={userReviews}
+      />
+    </div>
+  );
+};
+
+export default UserProfilePage;
---
+++ src/services/user/index.ts
@@ -0,0 +1,111 @@
+"use server";
+
+import { IUserResponse } from "@/types/user";
+import { revalidateTag } from "next/cache";
+import { cookies } from "next/headers";
+
+export const getAllUser = async () => {
+  try {
+    const res = await fetch(`${process.env.BASE_API}/user`, {
+      next: {
+        tags: ["USER"],
+      },
+      headers: {
+        Authorization: (await cookies()).get("accessToken")!.value,
+        "Content-Type": "application/json",
+      },
+    });
+    return await res.json();
+  } catch (error: any) {
+    throw Error(error);
+  }
+};
+
+export const getSingleUser = async (id: string) => {
+  try {
+    const res = await fetch(`${process.env.BASE_API}/user/${id}`, {
+      next: {
+        tags: ["USER"],
+      },
+      headers: {
+        Authorization: (await cookies()).get("accessToken")!.value,
+        "Content-Type": "application/json",
+      },
+    });
+    return await res.json();
+  } catch (error: any) {
+    throw Error(error);
+  }
+};
+
+export const deleteSingleUser = async (userId: string) => {
+  try {
+    const res = await fetch(
+      `${process.env.BASE_API}/user/${userId}`,
+
+      {
+        headers: {
+          Authorization: (await cookies()).get("accessToken")!.value,
+          "Content-Type": "application/json",
+        },
+        method: "DELETE",
+      }
+    );
+    revalidateTag("USER");
+
+    const data = await res.json();
+    return data;
+  } catch (error: any) {
+    return Error(error.message);
+  }
+};
+
+export const updateProfile = async (
+  userId: string,
+  updatedData: Partial<IUserResponse>
+) => {
+  try {
+    const res = await fetch(
+      `${process.env.BASE_API}/user/update-user/${userId}`,
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: (await cookies()).get("accessToken")!.value,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedData),
+      }
+    );
+
+    revalidateTag("USER");
+
+    return res.json();
+  } catch (error: any) {
+    return Error(error);
+  }
+};
+
+export const updatePassword = async (
+  userId: string,
+  updatedData: { prevPassword: string; newPassword: string }
+) => {
+  try {
+    const res = await fetch(
+      `${process.env.BASE_API}/user/update-password/${userId}`,
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: (await cookies()).get("accessToken")!.value,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedData),
+      }
+    );
+
+    revalidateTag("USER");
+
+    return res.json();
+  } catch (error: any) {
+    return Error(error);
+  }
+};
