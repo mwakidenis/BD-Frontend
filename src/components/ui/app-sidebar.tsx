fbc65e8a8a222ea4fc86397d053676420c05a836
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-09-15 14:08:47 +0600

    feat: app sidebar

--- src/components/ui/app-sidebar.tsx
+++ src/components/ui/app-sidebar.tsx
@@ -1,61 +1,60 @@
-// "use client";
+"use client";
 
-// import {
-//   Sidebar,
-//   SidebarContent,
-//   SidebarGroup,
-//   SidebarGroupContent,
-//   SidebarGroupLabel,
-//   SidebarMenu,
-//   SidebarMenuButton,
-//   SidebarMenuItem,
-// } from "@/components/ui/sidebar";
-// import { usePathname } from "next/navigation";
-// import Link from "next/link";
-// // import Logo from "../home/Logo"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
-// type TItem = {
-//   title: string;
-//   url: string;
-//   icon: React.ElementType;
-// };
+type TItem = {
+  title: string;
+  url: string;
+  icon: React.ElementType;
+};
 
-// export function AppSidebar({ items }: { items: TItem[] }) {
-//   const pathname = usePathname();
+export function AppSidebar({ items }: { items: TItem[] }) {
+  const pathname = usePathname();
 
-//   return (
-//     <Sidebar collapsible="icon">
-//       <SidebarContent>
-//         <SidebarGroup>
-//           <Link href="/">
-//             <Logo />
-//           </Link>
-//           <SidebarGroupLabel className="text-lg font-semibold py-6">
-//             <Link href="/">MediCommerce</Link>
-//           </SidebarGroupLabel>
-//           <SidebarGroupContent>
-//             <SidebarMenu className="gap-4">
-//               {items.map((item: TItem) => (
-//                 <SidebarMenuItem key={item.title}>
-//                   <SidebarMenuButton asChild>
-//                     <Link
-//                       href={item.url}
-//                       className={`${
-//                         pathname === item.url
-//                           ? "text-primary font-semibold"
-//                           : ""
-//                       }`}
-//                     >
-//                       <item.icon />
-//                       <span>{item.title}</span>
-//                     </Link>
-//                   </SidebarMenuButton>
-//                 </SidebarMenuItem>
-//               ))}
-//             </SidebarMenu>
-//           </SidebarGroupContent>
-//         </SidebarGroup>
-//       </SidebarContent>
-//     </Sidebar>
-//   );
-// }
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarContent>
+        <SidebarGroup>
+          <Link href="/">
+            <h1>SoptokBD</h1>
+          </Link>
+          <SidebarGroupLabel className="text-lg font-semibold py-6">
+            <Link href="/">MediCommerce</Link>
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu className="gap-4">
+              {items.map((item: TItem) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link
+                      href={item.url}
+                      className={`${
+                        pathname === item.url
+                          ? "text-primary font-semibold"
+                          : ""
+                      }`}
+                    >
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -15,15 +15,12 @@ export const registerUser = async (userData: FieldValues) => {
       },
       body: JSON.stringify(userData),
     });
-
     const result = await res.json();
 
     if (result.success) {
       (await cookies()).set("accessToken", result.data.accessToken);
-
       (await cookies()).set("refreshToken", result.data.refreshToken);
     }
-
     return result;
   } catch (error: any) {
     throw Error(error);
@@ -40,15 +37,11 @@ export const loginUser = async (userData: FieldValues) => {
       },
       body: JSON.stringify(userData),
     });
-
     const result = await res.json();
-
     if (result.success) {
       (await cookies()).set("accessToken", result.data.accessToken);
-
       (await cookies()).set("refreshToken", result.data.refreshToken);
     }
-
     return result;
   } catch (error: any) {
     return Error(error);
