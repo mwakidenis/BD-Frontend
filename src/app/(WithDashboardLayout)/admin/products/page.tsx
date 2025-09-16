c17a2a2019711afc337b00d851fc23f37ef895b3
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-09-16 13:45:08 +0600

    feat: manage all product

---
+++ src/app/(WithDashboardLayout)/admin/products/page.tsx
@@ -0,0 +1,26 @@
+export const dynamic = "force-dynamic";
+
+import ManageMedicine from "@/components/dashboard/admin/ManageProducts/ManageProducts";
+import { getAllMedicine } from "@/services/product";
+import React from "react";
+
+const MedicinesPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ searchItem: string }>;
+}) => {
+  const resolvedSearchParams = await searchParams;
+
+  // Merge searchParams with a default 'limit' if not present
+  const searchParamsWithLimit = { ...resolvedSearchParams };
+
+  const { data: medicines, meta } = await getAllMedicine(searchParamsWithLimit);
+
+  return (
+    <div className=" w-full">
+      <ManageMedicine data={medicines} meta={meta} />
+    </div>
+  );
+};
+
+export default MedicinesPage;
---
+++ src/components/dashboard/DashboardComponent/ProductSearchbar/ProductSearchbar.tsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
+const ProductSearchbar = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    // Set initial searchTerm from URL when component mounts
+    const initialSearch = searchParams.get("searchTerm") || "";
+    setSearchTerm(initialSearch);
+  }, [searchParams]);
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearchTerm(value);
+
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set("searchTerm", value);
+    } else {
+      params.delete("searchTerm");
+    }
+    router.push(`?${params.toString()}`, { scroll: false });
+  };
+
+  const handleReset = () => {
+    setSearchTerm("");
+    const params = new URLSearchParams(searchParams);
+    params.delete("searchTerm");
+    params.delete("page");
+    router.push(`?${params.toString()}`, { scroll: false });
+  };
+
+  return (
+    <div className="space-y-1 mb-6">
+      <Label htmlFor="search">Search (Name, Description)</Label>
+      <div className="flex items-center gap-2">
+        <Input
+          id="search"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+        {
+          <Button variant="secondary" onClick={handleReset}>
+            Reset
+          </Button>
+        }
+      </div>
+    </div>
+  );
+};
+
+export default ProductSearchbar;
