a6006a72f8fa984bacae53481ccff609245762d6
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-09-16 13:01:38 +0600

    feat: manage users page

---
+++ src/app/(WithDashboardLayout)/admin/users/page.tsx
@@ -0,0 +1,16 @@
+export const dynamic = "force-dynamic";
+
+import ManageUser from "@/components/dashboard/admin/ManageUsers/ManageUsers";
+import { getAllUser } from "@/services/user";
+import React from "react";
+
+const UsersPage = async () => {
+  const { data } = await getAllUser();
+  return (
+    <div className="overflow-x-auto w-full">
+      <ManageUser data={data} />
+    </div>
+  );
+};
+
+export default UsersPage;
