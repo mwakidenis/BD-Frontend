ebe95aac956a7e99561cfc59ccee5319c3b96241
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-12-23 22:22:06 +0600

    daily commit

--- src/app/(WithCommonLayout)/about/page.tsx
+++ src/app/(WithCommonLayout)/about/page.tsx
@@ -207,7 +207,7 @@ export default function AboutUs() {
       <div className="bg-gray-100 py-8">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
           <p className="text-gray-600">
-            Thank you for choosing SoptokBD. Together, we're preserving
+            Thank you for choosing SoptokBD Platform. Together, we're preserving
             traditions and creating memories.
           </p>
         </div>
